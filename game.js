377dbf1d035a021429c46196e93dd2a300420a3c
Peter Stevens <peterstevens510@gmail.com>
2020-11-30 18:27:08 +0000

    added road animation

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ var block = document.getElementById("block");
 var lastUpdated = new Date().getTime();
 
 document.addEventListener("keyup", function (e) {
-  if (e.keyCode === 32 && new Date().getTime() > lastUpdated + 450) {
+  if (e.keyCode === 32 && new Date().getTime() > lastUpdated + 400) {
     jump();
     lastUpdated = new Date().getTime();
   }
@@ -17,6 +17,8 @@ function jump() {
     character.classList.remove("jump-animate");
   }, 500); // Removes animation after 0.5 seconds after triggering it.
 }
+
+function reset() {}
 
 var checkDead = setInterval(function () {
   let characterTop = parseInt(
