9500ec901d1886c3ee518dbb9ab149439f4b6bca
Peter Stevens <peterstevens510@gmail.com>
2020-11-30 16:55:37 +0000

    collision now works, just updated CSS and adding additional features

--- game.js
+++ game.js
@@ -3,15 +3,31 @@ var block = document.getElementById("block");
 var lastUpdated = new Date().getTime();
 
 document.addEventListener("keyup", function (e) {
-  if (e.keyCode === 32 && new Date().getTime() > lastUpdated + 475) {
+  if (e.keyCode === 32 && new Date().getTime() > lastUpdated + 450) {
     jump();
     lastUpdated = new Date().getTime();
   }
 });
 
 function jump() {
-  character.classList.add("animate");
+  if (character.classList != "jump-animate") {
+    character.classList.add("jump-animate");
+  }
   setTimeout(function () {
-    character.classList.remove("animate");
+    character.classList.remove("jump-animate");
   }, 500); // Removes animation after 0.5 seconds after triggering it.
 }
+
+var checkDead = setInterval(function () {
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 90 && blockLeft > 0 && characterTop >= 140) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("You Crashed!");
+  }
+}, 10);
