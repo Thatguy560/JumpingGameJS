47ecd3f2ce7c13cd6d430f3faf3e74c2fa945570
Peter Stevens <peterstevens510@gmail.com>
2020-12-01 19:12:09 +0000

    reset button works, now just need to store the top score value after reset

--- game.js
+++ game.js
@@ -3,6 +3,18 @@ var block = document.getElementById("block");
 var lastUpdated = new Date().getTime();
 var counter = 0;
 var maxScore = [];
+const restartButton = document.getElementById("restartButton");
+
+// startGame();
+
+jump = () => {
+  if (character.classList != "jump-animate") {
+    character.classList.add("jump-animate");
+  }
+  setTimeout(function () {
+    character.classList.remove("jump-animate");
+  }, 500); // Removes animation after 0.5 seconds after triggering it.
+};
 
 document.addEventListener("keyup", function (e) {
   if (e.keyCode === 32 && new Date().getTime() > lastUpdated + 400) {
@@ -11,18 +23,9 @@ document.addEventListener("keyup", function (e) {
   }
 });
 
-function startGame() {}
-
-function jump() {
-  if (character.classList != "jump-animate") {
-    character.classList.add("jump-animate");
-  }
-  setTimeout(function () {
-    character.classList.remove("jump-animate");
-  }, 500); // Removes animation after 0.5 seconds after triggering it.
+function startGame() {
+  console.log("Start Game");
 }
-
-function reset() {}
 
 var checkDead = setInterval(function () {
   let characterTop = parseInt(
@@ -33,21 +36,27 @@ var checkDead = setInterval(function () {
   );
   if (blockLeft < 90 && blockLeft > 0 && characterTop >= 140) {
     block.style.animation = "none";
-    block.style.display = "none";
     line.style.animation = "none";
-    line.style.display = "none";
+    character.style.animation = "none";
     alert("You Crashed: Your score is " + Math.floor(counter / 100));
     maxScore.push(Math.floor(counter / 100));
     document.getElementById("scoreBoard").style.visibility = "hidden";
     document.getElementById("topScore").innerHTML = maxScore.sort(
       (a, b) => b - a
     )[0];
-    counter = 0;
+    // counter = 0;
   } else {
     counter++;
     document.getElementById("scoreBoard").innerHTML = Math.floor(counter / 100);
   }
 }, 10);
 
+restartButton.addEventListener("click", resetGame);
+
+function resetGame() {
+  console.log("test");
+}
+
 // document.getElementsByTagName("h2")[0].innerHTML = "Current Score: 0";
-// console.log(maxScore.sort((a, b) => b - a)[0]);
+// block.style.display = "none";
+// line.style.display = "none";
