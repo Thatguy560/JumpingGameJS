2bca69d5c99836ed696e94f30d1d84a9a345d8b7
Peter Stevens <peterstevens510@gmail.com>
2020-12-03 21:57:33 +0000

    added colours once score gets past a certain point

--- game.js
+++ game.js
@@ -19,6 +19,26 @@ document.addEventListener("keyup", function (e) {
   }
 });
 
+// moveDown = () => {
+//   character.style.top = 200 + "px";
+// };
+
+// document.addEventListener("keydown", function (e) {
+//   if (e.keyCode === 40) {
+//     moveDown();
+//   }
+// });
+
+// moveUp = () => {
+//   character.style.top = 150 + "px";
+// };
+
+// document.addEventListener("keyup", function (e) {
+//   if (e.keyCode === 38) {
+//     moveUp();
+//   }
+// });
+
 function increaseBlockSpeed() {
   let score = Math.floor(counter / 100);
   if (score === 0 || score < 10) {
@@ -29,24 +49,28 @@ function increaseBlockSpeed() {
   } else if (score === 30 || score < 51) {
     block.classList.remove("block-animate-2");
     block.classList.add("block-animate-3");
-  } else if (score === 51 || score < 102.8) {
+    document.getElementById("scoreBoard").style.color = "#cd7f32";
+  } else if (score === 51 || score < 100) {
     block.classList.remove("block-animate-3");
     block.classList.add("block-animate-4");
+    document.getElementById("scoreBoard").style.color = "#7c8288";
   } else {
     block.classList.remove("block-animate-4");
     block.classList.add("block-animate-max");
+    document.getElementById("scoreBoard").style.color = "#FFD700";
   }
 }
 
 function keepScore() {
-  var maxScore = JSON.parse(localStorage.maxScore || "[]");
-  maxScore.push(Math.floor(counter / 100));
-  localStorage.setItem("maxScore", JSON.stringify(maxScore));
-  var maxScore = localStorage.getItem("maxScore");
-  var maxScore = JSON.parse(maxScore);
-  document.getElementById("topScore").innerHTML = maxScore.sort((a, b) => {
+  var allScores = JSON.parse(localStorage.allScores || "[]");
+  allScores.push(Math.floor(counter / 100));
+  localStorage.setItem("allScores", JSON.stringify(allScores));
+  var allScores = localStorage.getItem("allScores");
+  var allScores = JSON.parse(allScores);
+  var maxScore = allScores.sort((a, b) => {
     return b - a;
   })[0];
+  document.getElementById("topScore").innerHTML = maxScore;
 }
 
 var checkDead = setInterval(function () {
@@ -62,7 +86,7 @@ var checkDead = setInterval(function () {
     character.style.animation = "none";
     document.getElementById("scoreBoard").style.visibility = "hidden";
     keepScore();
-    alert("You Crashed: Your score is " + Math.floor(counter / 100));
+    alert("You Crashed: You scored " + Math.floor(counter / 100));
   } else {
     counter++;
     increaseBlockSpeed();
